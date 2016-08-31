2e5e0bf9c603819f3234bbd05caf2f76336be061
hisheng <465908774@qq.com>
2016-08-31 18:08:40 +0800

    获取 http请求相关情况
    获取 http请求相关情况

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -11,60 +11,30 @@ class IndexController extends Yaf_Controller_Abstract {
 	}
 
 	public function indexAction() {
-
-
         echo 'index/index';
 
-
         //test
-
-
         echo \Monolog\Logger::DEBUG;
-
-
-
-
 
     }
 
     public function testAction(){
+        //获取 请求的实例
+        $http = $this->getRequest();
+        var_dump($http);
+
+        //获取控制器 实例
+
         echo 'index/test';
+        var_dump($http->getServer());
+        var_dump($http->get('name'));
+        var_dump($http->getCookie());
+        var_dump($http->getQuery());
+        var_dump($http->getPost());
+        var_dump($http->getParams());
     }
 
     public function redis2Action(){
-
-
-
-//        $isHide=$this->Redis_->hGet('gift_setad', 'isHide');
-//        if ($isHide == 'true') {
-//            $isHide1 = true;
-//        }else{
-//            $isHide1 = false;
-//        }
-//
-//
-//        $url=$this->Redis_->hGet('gift_setad', 'url');
-//        if (empty($url)) {
-//            $url1='';
-//        }else{
-//            $url1=$url;
-//        }
-//
-//
-//        $b=array();
-//        $b['isHide']=$isHide1;
-//        $b['url']=trim($url1);
-//        $b['giftId']='0';
-//        $b['count']=0;
-//        $b['imgurl']=$this->Redis_->hGet('gift_setad', 'imgurl');
-//        print_r(json_encode($b));
-//
-
-
-      /*  $a = new \HttpFoundation\Request();
-        $a->get();*/
-
-
     }
 
 }
